ad20535429773c321fa31df4f6f456ff74611ad0
chenzelong <chenzelong@soulapp.cn>
2023-02-10 18:56:10 +0800

    feat: support del history

--- src/components/history.vue
+++ src/components/history.vue
@@ -6,32 +6,22 @@
       <div class="th">首付（含税）</div>
       <div class="th">月供</div>
     </div>
-    <div class="tr" v-for="(value, key) in list">
-      <div class="tr-top">
-        <div class="td">{{ value.price }}<span>万</span></div>
-        <div class="td">{{ value.area }}<span>m²</span></div>
-        <div class="td alert">{{ parseInt(value.taxWithDownPayment) }}({{ parseInt(value.tax) }})<span>万</span></div>
-        <div class="td">{{ parseInt(value.totalLoan.wayOfPI) }}<span>元</span></div>
-      </div>
-      <div class="tr-bottom">
-        <div class="tr-bottom-left">{{ value.uuid }}</div>
-        <div class="tr-bottom-right"><strong>63257</strong>元/m²</div>
-      </div>
-    </div>
+    <history-item v-for="(value) in list" :value="value"></history-item>
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted } from "vue"
 import history from "../utils/history"
-import store from '../utils/store';
-import { STORE_HOUSE } from "../utils/bus"
-import { bus, BUS_EVENT, form, house } from "../utils/bus"
+import { bus, BUS_EVENT } from "../utils/bus"
+import historyItem from "./history-item.vue"
 
 const list = history.list
 
-onMounted(() => bus.emit(BUS_EVENT.HISTORY_INIT))
+onMounted(() => {
+  bus.emit(BUS_EVENT.HISTORY_INIT)
+})
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 .table {
   position: absolute;
   width: 100%;
@@ -88,81 +78,6 @@ onMounted(() => bus.emit(BUS_EVENT.HISTORY_INIT))
   flex: 1;
   &::after {
     display: none;
-  }
-}
-
-.tr {
-  width: 703px;
-  height: 156px;
-  margin: 22px auto;
-  background: #ededed;
-  box-shadow: 0px 10px 21px 0px rgba(0, 0, 0, 0.15);
-  border-radius: 32px;
-  padding: 35px 20px 26px;
-
-  &-top {
-    display: flex;
-  }
-
-  &-bottom {
-    width: 662px;
-    height: 48px;
-    display: flex;
-    align-items: center;
-    background: #e0e1e5;
-    border-radius: 24px;
-    margin: 22px auto 0;
-    font-size: 26px;
-    font-weight: 500;
-    color: #a2a2a2;
-
-    &-left {
-      position: relative;
-      flex: 1;
-      padding-left: 20px;
-      padding-right: 15px;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      white-space: nowrap;
-
-      &::after {
-        position: absolute;
-        content: "";
-        width: 2px;
-        height: 28px;
-        right: 0;
-        top: 50%;
-        transform: translate3d(0, -50%, 0);
-        background-color: #A2A2A2;
-      }
-    }
-
-    &-right {
-      padding-left: 15px;
-      flex-shrink: 0;
-      flex-basis: 230px;
-      strong {
-        color: #5C5C5C;
-      }
-    }
-  }
-}
-
-.alert {
-  color: var(--color-primary) !important;
-}
-
-.td {
-  font-size: 32px;
-  font-weight: 600;
-  color: #5c5c5c;
-  text-align: center;
-
-  span {
-    margin-left: 4px;
-    font-size: 22px;
-    font-weight: 600;
-    color: #afafaf;
   }
 }
 </style>
---
+++ src/utils/swiper.ts
@@ -0,0 +1,66 @@
+export default class SwipeDelete {
+  private startX: number
+  private currentX: number
+  private startY: number
+  private currentY: number
+  private itemWidth: number
+  private confirmWidth: number
+  private contentDom: HTMLElement
+  private delDom: HTMLElement
+
+  constructor(private uuid: string, private container: HTMLElement, private delFn: () => void) {
+    this.startX = 0
+    this.currentX = 0
+    this.startY = 0
+    this.currentY = 0
+    this.itemWidth = container.getBoundingClientRect().width
+    this.confirmWidth = this.itemWidth / 3
+    this.contentDom = this.container.querySelector(".tr-content") as HTMLElement
+    this.delDom = this.container.querySelector(".tr-del") as HTMLElement
+    container.ontouchstart = this.handleTouchStart
+    container.ontouchmove = this.handleTouchMove
+    container.ontouchend = this.handleTouchEnd
+  }
+
+  private moveContainer = (deltaX: number) => {
+    if (deltaX > 0) deltaX = 0
+    this.contentDom.style.transform = `translate3d(${deltaX}px, 0, 0)`
+    if (-deltaX > this.confirmWidth) {
+      this.delDom.classList.add("active")
+    } else {
+      this.delDom.classList.remove("active")
+    }
+  }
+
+  private handleTouchStart = (event: TouchEvent) => {
+    this.startX = event.touches[0].clientX
+    this.startY = event.touches[0].clientY
+  }
+
+  private handleTouchMove = (event: TouchEvent) => {
+    this.currentX = event.touches[0].clientX
+    this.currentY = event.touches[0].clientY
+    const deltaX = this.currentX - this.startX
+    const deltaY = this.currentY - this.startY
+    this.moveContainer(deltaX)
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (event.cancelable) event.preventDefault()
+    }
+  }
+
+  private handleTouchEnd = (event: TouchEvent) => {
+    const deltaX = this.currentX - this.startX
+    if (-deltaX > this.confirmWidth) this.removeItem()
+    this.reset()
+  }
+
+  private removeItem = () => {
+    this.delFn()
+  }
+
+  private reset = () => {
+    this.startX = 0
+    this.currentX = 0
+    this.moveContainer(0)
+  }
+}
